1a524654271fa1cdf7c4469cc5bee4e930369d9a
Rubyboat <52091447+Rubyboat1207@users.noreply.github.com>
2024-10-15 01:12:58 -0500

    remove base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { plugin as markdownPlugin } from 'vite-plugin-markdown'
 
 export default defineConfig({
-  base: '/markdown-portfolio/',
   plugins: [
     markdownPlugin({
       mode: 'html'
